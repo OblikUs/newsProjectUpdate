12d1435ec94e1365745e25ae4348df392fc7a8a5
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-02-21 11:52:34 -1000

    fixing 504 error

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 3000;
-const request = require('request');
 const cron = require('node-cron');
-const parseData = require('./parseData');
 const got = require('got');
-const querystring = require('querystring');
+
 const Promise = require('bluebird');
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-
-let url = [
+let urls = [
            "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            "https://newsapi.org/v1/articles?source=abc-news-au&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            "https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
@@ -22,9 +19,9 @@ let url = [
            "https://newsapi.org/v1/articles?source=business-insider-uk&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            "https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            "https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            // "https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            // "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            // "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
@@ -57,34 +54,37 @@ let url = [
            // "https://newsapi.org/v1/articles?source=time&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            // "https://newsapi.org/v1/articles?source=usa-today&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            ]
+while(urls.length > 0) {
+  console.log('urls.length: ', urls.length);
+  console.log('IN WHILE LOOP');
+  let newsApiSourceUrls = urls.splice(0,3);
 
+  // cron.schedule('43,44 19 * * *', () => {
 
-// cron.schedule('43,44 19 * * *', () => {
+  // })
 
-// })
-
-let files = [];
-let newsApiUrls = [];
-for(var i = 0; i < url.length; i++) {
-  files.push(got(url[i]));
-}
-Promise.map(files, (x) => {
-  return x.body;
-})
-.then(response => {
-  for(var i = 0; i < response.length; i++) {
-    let data = JSON.parse(response[i]);
-    for(var j = 0; j < data.articles.length; j++) {
-      let newUrls = encodeURIComponent(data.articles[j].url.trim());
-      newsApiUrls.push(newUrls);
-    }
+  let sendToNewsApi = [];
+  let newsApiStoryUrls = [];
+  for(var i = 0; i < newsApiSourceUrls.length; i++) {
+    sendToNewsApi.push(got(newsApiSourceUrls[i]));
   }
-})
+  Promise.map(sendToNewsApi, (x) => {
+    return x.body;
+  })
+  .then(response => {
+    for(var i = 0; i < response.length; i++) {
+      let data = JSON.parse(response[i]);
+      for(var j = 0; j < data.articles.length; j++) {
+        let newUrls = encodeURIComponent(data.articles[j].url.trim());
+        newsApiStoryUrls.push(newUrls);
+      }
+    }
+  })
   .then(() => {
     let newFiles = [];
-    for(var p = 0; p < newsApiUrls.length; p++) {
+    for(var p = 0; p < newsApiStoryUrls.length; p++) {
 
-      newFiles.push(got(`https://joanfihu-article-analysis-v1.p.mashape.com/link?entity_description=False&link=${newsApiUrls[p]}`, {
+      newFiles.push(got(`https://joanfihu-article-analysis-v1.p.mashape.com/link?entity_description=False&link=${newsApiStoryUrls[p]}`, {
                       headers: {
                         "X-Mashape-Key": "PRcyspm59vmsh7X8ue7NfZFzZz7op1oAfxsjsnCLMHQkRfnvUL",
                       },
@@ -96,15 +96,19 @@ Promise.map(files, (x) => {
             return t.body;
           })
   })
-  .then(response => {
-    for(var h = 0; h <response.length; h++) {
-      console.log('h: ', h);
-      console.log('response[h].body: ', response[h].title);
-    }
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(response => {
+      console.log('response: ', response);
+      for(var h = 0; h <response.length; h++) {
+        console.log('h: ', h);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .catch(SyntaxError, (e) => {
+      console.error('Invalid JSON');
+    });
+}
 
 
 
