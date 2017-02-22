2daa4262090f97442bee7fc4909a1e7da2b8021c
Estefania Morton <estefania.morton@gmail.com>
2017-02-21 19:58:37 -1000

    working on modifying data

---
+++ modifyData.js
@@ -0,0 +1,72 @@
+module.exports = (() => {
+
+  let sources = [ { source: 'us.cnn.com', name: 'CNN', view: 'center-left' },
+  { source: 'www.abc.net.au', name: 'ABC - Australian Broadcasting Corporation', view: '' },
+  { source: 'bigstory.ap.org', name: 'Associated Press - The Big Story', view: 'center' },
+  { source: 'www.bbc.co.uk', name: 'BBC', view: 'center-left' },
+  { source: 'www.bloomberg.com', name: 'Bloomberg', view: 'center-left' },
+  { source: 'www.businessinsider.com', name: 'Business Insider', view: 'center' },
+  { source: 'uk.businessinsider.com', name: 'Business Insider UK', view: 'center' },
+  { source: 'www.buzzfeed.com', name: 'Buzzfeed', view: 'left' },
+  { source: 'www.dailymail.co.uk', name: 'DailyMail', view: 'center-right' },
+  { source: 'www.engadget.com', name: 'Engadget', view: 'left' },
+  { source: 'ew.com', name: 'Entertainment Weekly', view: 'n/a' },
+  { source: 'espn.go.com', name: 'ESPN', view: 'n/a' },
+  { source: 'www.espn.com', name: 'ESPN', view: 'n/a' },
+  { source: 'www.foxsports.com', name: 'Fox Sports', view: 'n/a' },
+  { source: 'www.washingtonpost.com', name: 'The Washington Post', view: 'center-left' },
+  { source: 'thehill.com', name: 'The Hill', view: 'right' },
+  { source: 'www.chron.com', name: 'Chron - Houston Chronicle', view: 'right' },
+  { source: 'techcrunch.com', name: 'Techcrunch', view: 'n/a' },
+  { source: 'www.ign.com', name: 'IGN', view: 'n/a' },
+  { source: 'www.mirror.co.uk', name: 'Mirror', view: 'center-left' },
+  { source: 'mashable.com', name: 'Mashable', view: 'n/a' },
+  { source: 'www.mtv.com', name: 'MTV', view: 'left' },
+  { source: 'news.nationalgeographic.com', name: 'National Geographic', view: 'n/a' },
+  { source: 'www.newscientist.com', name: 'New Scientsit', view: 'n/a'},
+  { source: 'www.newsweek.com', name: 'Newsweek', view: '' },
+  { source: 'www.vulture.com', name: 'Vulture', view: 'left' },
+  { source: 'nymag.com', name: '', view: '' },
+  { source: 'www.nfl.com', name: '', view: 'n/a' },
+  { source: 'www.recode.net', name: '', view: '' },
+  { source: 'www.vox.com', name: 'Vox', view: 'left' },
+  { source: 'www.theverge.com', name: '', view: 'left' },
+  { source: 'www.politico.com', name: '', view: '' },
+  { source: 'www.hollywoodreporter.com', name: '', view: '' },
+  { source: 'feeds.reuters.com', name: '', view: '' },
+  { source: 'news.sky.com', name: '', view: '' },
+  { source: 'www.techradar.com', name: '', view: '' },
+  { source: 'www.economist.com', name: 'The Economist', view: 'center-right' },
+  { source: 'www.theguardian.com', name: 'The Guardian', view: 'center-left' },
+  { source: 'www.huffingtonpost.com', name: '', view: 'left' },
+  { source: 'www.theladbible.com', name: 'The Lad Bible', view: '' },
+  { source: 'www.nytimes.com', name: 'The New York Times', view: 'center-left' },
+  { source: 'thenextweb.com', name: 'The Next Web', view: 'center-left' },
+  { source: 'www.telegraph.co.uk', name: '', view: '' },
+  { source: 'www.wsj.com', name: '', view: '' },
+  { source: 'www.washingtonpost.com', name: 'The Washington Post', view: '' },
+  { source: 'time.com', name: '', view: '' },
+  { source: 'fortune.com', name: '', view: '' },
+  { source: 'usatoday.com', name: '', view: '' }
+  ]
+
+  const sourceNameFinder = (url) => {
+
+  }
+
+  const politicalView = (source) => {
+
+  }
+
+  const keywordGenerator = (entities, tags) => {
+
+  }
+
+  return {
+    sourceNameFinder,
+    politicalView,
+
+  }
+
+
+})();
