47190756c3c18c6c8ebd5126ef14bfff30027b34
jhoun <jayhoun@gmail.com>
2017-02-21 16:45:47 -1000

    implemented navigation bar

--- frontEndServer/app/static/Header.js
+++ frontEndServer/app/static/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
-// import styles from './Header.scss';
+import styles from './Header.scss';
 // import { Link } from 'react-router';
 
 const Header = () => (
-    <div> Hello </div>
+  <div className={styles.container}>
+      <div className={styles.logo}><a href='#'>Oblik</a></div>
+    <ul className={styles.mainNav}>
+      <li><a href="#">How it Works</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Install Oblik</a></li>
+    </ul>
+  </div>
 );
 
 export default Header;
