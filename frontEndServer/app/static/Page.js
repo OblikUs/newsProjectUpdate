1ed54b05586edfba26e89cfc332cd48478b2b7d6
jhoun <jayhoun@gmail.com>
2017-02-22 14:34:25 -1000

    implemented body

--- frontEndServer/app/static/Page.js
+++ frontEndServer/app/static/Page.js
@@ -13,8 +13,46 @@ class Page extends React.Component {
               <a href="#" className={styles.btn}>Download Now</a>
         </div>
         <div className={styles.imgContainer}>
-          <div className={styles.newsExplain}>Over 70+ news sources</div>
+          <div className={styles.newsExplain}>Over 50+ news sources</div>
           <img src={'http://i.imgur.com/aFZOJfT.png'} alt="news_articles" className={styles.newsLogos}/>
+        </div>
+        <div className={styles.row}>
+          <div className={[styles.primary, styles.col].join(' ')}>
+            <h2>Click and See</h2>
+
+            <p>Cupcake ipsum dolor sit. Amet chocolate cake gummies jelly beans candy bonbon brownie candy. Gingerbread powder muffin. Icing cotton candy. Croissant icing pie ice cream brownie I love cheesecake cookie. Pastry chocolate pastry jelly croissant.</p>
+
+          </div>
+
+          <div className={[styles.secondary, styles.col].join(' ')}>
+            <div className={styles.screenshot}>ScreenShot</div>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={[styles.primary, styles.col].join(' ')}>
+            <h2>Pick a side</h2>
+
+
+            <p>Cake sesame snaps sweet tart candy canes tiramisu I love oat cake chocolate bar. Jelly beans pastry brownie sugar plum pastry bear claw tiramisu tootsie roll. Tootsie roll wafer I love chocolate donuts.</p>
+
+          </div>
+
+          <div className={[styles.secondary, styles.col].join(' ')}>
+            <div className={styles.screenshot}>ScreenShot</div>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={[styles.primary, styles.col].join(' ')}>
+            <h2>Want to see more?</h2>
+
+            <p>Brownie ice cream cotton candy liquorice cake. Macaroon caramels fruitcake lemon drops. Carrot cake jelly halvah biscuit. </p>
+          </div>
+
+          <div className={[styles.secondary, styles.col].join(' ')}>
+            <div className={styles.screenshot}>ScreenShot</div>
+          </div>
         </div>
       </div>
 
