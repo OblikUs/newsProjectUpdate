095e1a0a2a3549a008dc665bc221f23e22c3f467
Estefania Morton <estefania.morton@gmail.com>
2017-02-23 10:48:49 -1000

    merging with frontend

--- index.js
+++ index.js
@@ -1,59 +1,81 @@
-const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const neo4j = require('neo4j-driver').v1;
+const neo4jUrl = "http://localhost:7474/db/data/transaction/commit";
+const authUrl = "http://localhost:7474/user/neo4j";
 const cron = require('node-cron');
 const got = require('got');
-// const md = require('./modifyData')
-
+const r = require("request");
+const md = require('./modifyData')
+const usernameAndPwd = new Buffer("neo4j:c15finalP").toString('base64');
 const Promise = require('bluebird');
 
-app.use(bodyParser.urlencoded({extended:true}))
+
+function cypher(query, params, cb) {
+  r.post({
+    uri:neo4jUrl,
+    headers:{Authorization: usernameAndPwd},
+    json:{statements:[{statement:query, parameters:params }]}},
+    function(err,res) {
+      cb(err,res.body)
+    })
+}
+
+let query = `
+WITH {json} AS data
+UNWIND data.data as x
+MERGE (article:Article {title: x.title}) ON CREATE
+  SET article.source = x.name, article.url = x.url, article.view = x.view
+
+FOREACH (keywordWord IN x.keywords | MERGE (keyword:Keyword {word: keywordWord}) MERGE (article)-[:HAS_KEYWORD]->(keyword))
+`;
+
+let json = {data:[]};
+// let data = [];
 
 let urls = [
-           // "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=abc-news-au&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=business-insider-uk&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=independent&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=mirror&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=mashable&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=mtv-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=new-scientist&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=newsweek&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=new-york-magazine&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=reuters&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=sky-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=techradar&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-economist&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-guardian-au&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-lad-bible&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-telegraph&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=time&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
-           // "https://newsapi.org/v1/articles?source=usa-today&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=abc-news-au&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=business-insider-uk&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=independent&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=mirror&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=mashable&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=mtv-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=new-scientist&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=newsweek&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=new-york-magazine&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=reuters&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=sky-news&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=techradar&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-economist&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-guardian-au&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-lad-bible&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-telegraph&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=time&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
+           "https://newsapi.org/v1/articles?source=usa-today&sortBy=top&apiKey=9f3b3102ab704b7c9a874ee92cdb288f",
            ]
 
 // cron.schedule('43,44 19 * * *', () => {
@@ -74,7 +96,6 @@ Promise.map(urls, (topStoryUrl) => {
 }, [])
 .map((topStory) => {
   let encodedUrl = encodeURIComponent(topStory.url.trim());
-  // console.log(encodedUrl)
   return got(
     'https://joanfihu-article-analysis-v1.p.mashape.com/link?' +
     'entity_description=False&' +
@@ -92,7 +113,23 @@ Promise.map(urls, (topStoryUrl) => {
   return typeof NLPResult !== 'undefined';
 })
 .each((NLPResult) => {
-  console.log('NLPResult.body: ', NLPResult.body.link);
+  // console.log('NLPResult.body: ', NLPResult.body.link);
+  let source = md.sourceFinder(NLPResult.body.link);
+  if(source.length === 0) {
+    if(NLPResult.body.author.length === 1) {
+      source = [{source: NLPResult.body.author, name: NLPResult.body.author[0], view: 'n/a'}]
+    } else {
+      source = [{source: NLPResult.body.author, name: 'unknown', view: 'n/a'}]
+    }
+  }
+  let keywords = md.keywordGenerator(NLPResult.body.entities, NLPResult.body.tags)
+  json.data.push(md.newData(NLPResult.body.title, source.name, source.view, NLPResult.body.link, keywords))
+})
+.then(() => {
+  json = JSON.stringify(json);
+  // data = JSON.stringify(data);
+  json = (JSON.parse(json));
+  cypher(query, {json:json}, function(err, result) { console.log(err, JSON.stringify(result))});
 })
 .catch(error => {
   console.log(error);
--- modifyData.js
+++ modifyData.js
@@ -1,71 +1,49 @@
+const sources = require('./sources')
+
+// FIX SKY.NEWS
+
 module.exports = (() => {
 
-  let sources = [ { source: 'us.cnn.com', name: 'CNN', view: 'center-left' },
-  { source: 'www.abc.net.au', name: 'ABC - Australian Broadcasting Corporation', view: '' },
-  { source: 'bigstory.ap.org', name: 'Associated Press - The Big Story', view: 'center' },
-  { source: 'www.bbc.co.uk', name: 'BBC', view: 'center-left' },
-  { source: 'www.bloomberg.com', name: 'Bloomberg', view: 'center-left' },
-  { source: 'www.businessinsider.com', name: 'Business Insider', view: 'center' },
-  { source: 'uk.businessinsider.com', name: 'Business Insider UK', view: 'center' },
-  { source: 'www.buzzfeed.com', name: 'Buzzfeed', view: 'left' },
-  { source: 'www.dailymail.co.uk', name: 'DailyMail', view: 'center-right' },
-  { source: 'www.engadget.com', name: 'Engadget', view: 'left' },
-  { source: 'ew.com', name: 'Entertainment Weekly', view: 'n/a' },
-  { source: 'espn.go.com', name: 'ESPN', view: 'n/a' },
-  { source: 'www.espn.com', name: 'ESPN', view: 'n/a' },
-  { source: 'www.foxsports.com', name: 'Fox Sports', view: 'n/a' },
-  { source: 'www.washingtonpost.com', name: 'The Washington Post', view: 'center-left' },
-  { source: 'thehill.com', name: 'The Hill', view: 'right' },
-  { source: 'www.chron.com', name: 'Chron - Houston Chronicle', view: 'right' },
-  { source: 'techcrunch.com', name: 'Techcrunch', view: 'n/a' },
-  { source: 'www.ign.com', name: 'IGN', view: 'n/a' },
-  { source: 'www.mirror.co.uk', name: 'Mirror', view: 'center-left' },
-  { source: 'mashable.com', name: 'Mashable', view: 'n/a' },
-  { source: 'www.mtv.com', name: 'MTV', view: 'left' },
-  { source: 'news.nationalgeographic.com', name: 'National Geographic', view: 'n/a' },
-  { source: 'www.newscientist.com', name: 'New Scientsit', view: 'n/a'},
-  { source: 'www.newsweek.com', name: 'Newsweek', view: '' },
-  { source: 'www.vulture.com', name: 'Vulture', view: 'left' },
-  { source: 'nymag.com', name: '', view: '' },
-  { source: 'www.nfl.com', name: '', view: 'n/a' },
-  { source: 'www.recode.net', name: '', view: '' },
-  { source: 'www.vox.com', name: 'Vox', view: 'left' },
-  { source: 'www.theverge.com', name: '', view: 'left' },
-  { source: 'www.politico.com', name: '', view: '' },
-  { source: 'www.hollywoodreporter.com', name: '', view: '' },
-  { source: 'feeds.reuters.com', name: '', view: '' },
-  { source: 'news.sky.com', name: '', view: '' },
-  { source: 'www.techradar.com', name: '', view: '' },
-  { source: 'www.economist.com', name: 'The Economist', view: 'center-right' },
-  { source: 'www.theguardian.com', name: 'The Guardian', view: 'center-left' },
-  { source: 'www.huffingtonpost.com', name: '', view: 'left' },
-  { source: 'www.theladbible.com', name: 'The Lad Bible', view: '' },
-  { source: 'www.nytimes.com', name: 'The New York Times', view: 'center-left' },
-  { source: 'thenextweb.com', name: 'The Next Web', view: 'center-left' },
-  { source: 'www.telegraph.co.uk', name: '', view: '' },
-  { source: 'www.wsj.com', name: '', view: '' },
-  { source: 'www.washingtonpost.com', name: 'The Washington Post', view: '' },
-  { source: 'time.com', name: '', view: '' },
-  { source: 'fortune.com', name: '', view: '' },
-  { source: 'usatoday.com', name: '', view: '' }
-  ]
-
-  const sourceNameFinder = (url) => {
-
-  }
-
-  const politicalView = (source) => {
-
+  const sourceFinder = (url) => {
+    url = url.split('http://').join('').split('.')
+    if(url[0].substring(0, 8) === 'https://') {
+      url[0] = url[0].substring(8)
+    }
+    url.map( (item, i) => {
+      if( item.length === 1 || item === 'www' || item === 'uk' || item ==='co' || item ==='com' || item ==='us' || item === 'money' || item === 'https://www'|| item === 'rssfeeds') {
+        url.splice(i, 1)
+      }
+    })
+    return sources.filter( source => {
+      if(source.source.search(url[0]) !== -1) {
+        // console.log(source);
+        return source
+      }
+    })
   }
 
   const keywordGenerator = (entities, tags) => {
+    entities = entities.map( entity => { return entity.toLowerCase()});
+    keywords = tags.filter( tag => {
+      return entities.indexOf(tag) === -1
+    }).concat(entities)
+    return keywords
+  }
 
+  const newData = (title, source, view, url, keywords) => {
+    return {
+      title,
+      source,
+      view,
+      url,
+      keywords
+    }
   }
 
   return {
-    sourceNameFinder,
-    politicalView,
-
+    sourceFinder,
+    keywordGenerator,
+    newData
   }
 
 
