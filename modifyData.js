b3e8f574927ae06d1f170394af1879e7c06ff290
Estefania Morton <estefania.morton@gmail.com>
2017-02-25 12:12:44 -1000

    image to db

--- modifyData.js
+++ modifyData.js
@@ -6,51 +6,47 @@ const stopWords = require('./stopWords')
 module.exports = (() => {
 
   const sourceFinder = (url) => {
-    url = url.split('http://').join('').split('.')
-    if(url[0].substring(0, 8) === 'https://') {
-      url[0] = url[0].substring(8)
-    }
-    url.map( (item, i) => {
-      if( item.length === 1 || item === 'www' || item === 'uk' || item ==='co' || item ==='com' || item ==='us' || item === 'money' || item === 'https://www'|| item === 'rssfeeds') {
-        url.splice(i, 1)
-      }
-    })
+    url = url.replace(/\w+:\/\//, '');
+    url = url.replace(/\/.*/, '')
     return sources.filter( source => {
-      if(source.source.search(url[0]) !== -1) {
+      if(source.source === url) {
         return source
       }
     })
   }
 
   const keywordGenerator = (title, description) => {
-    title = title.toLowerCase().split(' ');
-    description  = description.toLowerCase().split(' ');
-    let kws = title.filter( word => {
-      return stopWords.indexOf(word) === -1
-    }).concat(
-      description.filter( word => {
-        return stopWords.indexOf(word) === -1 && title.indexOf(word) === -1
-      })
-    ).map( word => {
-      let punctuations = ['.', ',', "\'", "'", '?', '!', ':', ';', "\""]
-      let splitW = word.split('').filter((letter, i) => {
-        return punctuations.indexOf(letter) === -1
-      })
-      word = splitW.join('')
-      return word
-
-    })
-    return kws
+    // title = title.toLowerCase().split(' ');
+    title = title.toLowerCase()
+    title = title.replace( /[.,\/#!$%\^&\*;:{}=\_`~"'…|()?]/, '')
+    return title;
+    // description  = description.toLowerCase().split(' ');
+    // let kws = title.filter( word => {
+    //   return stopWords.indexOf(word) === -1
+    // }).concat(
+    //   description.filter( word => {
+    //     return stopWords.indexOf(word) === -1 && title.indexOf(word) === -1
+    //   })
+    // ).map( word => {
+    //   // word = word.replace( /[.,\/#!$%\^&\*;:{}=\_`~"'…|()?]/, "")
+    //   word = word.replace( /\W/, "")
+    //   word = word.replace(/('s')/, "")
+    //   return word
+    // }).filter(word => {
+    //   return word !== ''
+    // })
+    // return kws
   }
 
-  const newData = (title, source, view, url, keywords, date) => {
+  const newData = (title, source, view, url, keywords, date, image) => {
     return {
       title,
       source,
       view,
       url,
       keywords,
-      date
+      date,
+      image
     }
   }
 
