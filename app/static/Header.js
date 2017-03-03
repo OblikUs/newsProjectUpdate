3624a1aa2ca5e252d2f102a8ab5a22a43cda46fb
Estefania Morton <estefania.morton@gmail.com>
2017-03-02 15:23:47 -1000

    mobile styles mvp

--- app/static/Header.js
+++ app/static/Header.js
@@ -5,10 +5,16 @@ import { Link } from 'react-router';
 const Header = () => (
   <div className={styles.container}>
       <div className={styles.logo}><a href='#'>Oblik</a></div>
+      <div className={styles.menuBtn}></div>
     <ul className={styles.mainNav}>
+      <li>Menu</li>
+      <hr/>
       <li><Link to="/how-it-works">How it Works</Link></li>
+      <hr/>
       <li><Link to="/about-us">About Us</Link></li>
+      <hr/>
       <li><a href="#">Install Oblik</a></li>
+      <hr/>
     </ul>
   </div>
 );
--- app/static/Page.js
+++ app/static/Page.js
@@ -16,41 +16,43 @@ class Page extends React.Component {
           <div className={styles.newsExplain}>Over 50+ news sources</div>
           <img src={'http://i.imgur.com/aFZOJfT.png'} alt="news_articles" className={styles.newsLogos}/>
         </div>
-        <div className={styles.row}>
-          <div className={[styles.primary, styles.col].join(' ')}>
-            <h2>Click and See</h2>
+        <div className={styles.rowContainer}>
+          <div className={styles.row}>
+            <div className={[styles.primary, styles.col].join(' ')}>
+              <h2>Click and See</h2>
 
-            <p>Cupcake ipsum dolor sit. Amet chocolate cake gummies jelly beans candy bonbon brownie candy. Gingerbread powder muffin. Icing cotton candy. Croissant icing pie ice cream brownie I love cheesecake cookie. Pastry chocolate pastry jelly croissant.</p>
+              <p>Cupcake ipsum dolor sit. Amet chocolate cake gummies jelly beans candy bonbon brownie candy. Gingerbread powder muffin. Icing cotton candy. Croissant icing pie ice cream brownie I love cheesecake cookie. Pastry chocolate pastry jelly croissant.</p>
 
+            </div>
+
+            <div className={[styles.secondary, styles.col].join(' ')}>
+              <div className={styles.screenshot}>ScreenShot</div>
+            </div>
           </div>
 
-          <div className={[styles.secondary, styles.col].join(' ')}>
-            <div className={styles.screenshot}>ScreenShot</div>
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={[styles.primary, styles.col].join(' ')}>
-            <h2>Pick a side</h2>
+          <div className={styles.row}>
+            <div className={[styles.primary, styles.col].join(' ')}>
+              <h2>Pick a side</h2>
 
 
-            <p>Cake sesame snaps sweet tart candy canes tiramisu I love oat cake chocolate bar. Jelly beans pastry brownie sugar plum pastry bear claw tiramisu tootsie roll. Tootsie roll wafer I love chocolate donuts.</p>
+              <p>Cake sesame snaps sweet tart candy canes tiramisu I love oat cake chocolate bar. Jelly beans pastry brownie sugar plum pastry bear claw tiramisu tootsie roll. Tootsie roll wafer I love chocolate donuts.</p>
 
-          </div>
+            </div>
 
-          <div className={[styles.secondary, styles.col].join(' ')}>
-            <div className={styles.screenshot}>ScreenShot</div>
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={[styles.primary, styles.col].join(' ')}>
-            <h2>Want to see more?</h2>
-            <p>Check out how our project works! </p>
+            <div className={[styles.secondary, styles.col].join(' ')}>
+              <div className={styles.screenshot}>ScreenShot</div>
+            </div>
           </div>
 
-          <div className={[styles.secondary, styles.col].join(' ')}>
-            <div className={styles.screenshot}><img src={"https://media.giphy.com/media/3og0IFNBqItdxx2oJq/giphy.gif"} className={styles.neo4jgif} alt="neo4j gif"/></div>
+          <div className={styles.row}>
+            <div className={[styles.primary, styles.col].join(' ')}>
+              <h2>Want to see more?</h2>
+              <p>Check out how our project works! </p>
+            </div>
+
+            <div className={[styles.secondary, styles.col].join(' ')}>
+              <div className={styles.screenshot}><img src={"https://media.giphy.com/media/3og0IFNBqItdxx2oJq/giphy.gif"} className={styles.neo4jgif} alt="neo4j gif"/></div>
+            </div>
           </div>
         </div>
       </div>
