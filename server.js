b15ed7b2c3bee0c14504bba45816c42e422ab998
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-03-04 11:46:33 -1000

    fixed server https

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
@@ -58,13 +58,13 @@ if (isDeveloping) {
 }
 
 //SSL so that we can take URL from Https
-let secureServer = http.createServer(
-    // key: fs.readFileSync('./ssl/server.key'),
-    // cert: fs.readFileSync('./ssl/server.crt'),
-    // ca: fs.readFileSync('./ssl/ca.crt'),
-    // requestCert: true,
-    // rejectUnauthorized: false
- app).listen(port, function() {
+let secureServer = https.createServer({
+    key: fs.readFileSync('./ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.crt'),
+    ca: fs.readFileSync('./ssl/ca.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+ },app).listen(port, function() {
     console.log(`Secure Express server listening on port ${port}`);
 });
 
